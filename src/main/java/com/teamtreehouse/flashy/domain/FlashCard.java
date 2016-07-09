cb5e1eec27fb400382d16bdee23d0911267e75f9
craigsdennis <craig@teamtreehouse.com>
2016-07-08 23:20:28 -0700

    Adds model

---
+++ src/main/java/com/teamtreehouse/flashy/domain/FlashCard.java
@@ -0,0 +1,46 @@
+package com.teamtreehouse.flashy.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class FlashCard {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String term;
+  private String definition;
+
+  protected FlashCard() {
+    id = null;
+  }
+
+  public FlashCard(String term, String definition) {
+    this();
+    this.term = term;
+    this.definition = definition;
+  }
+
+  public String getTerm() {
+    return term;
+  }
+
+  public void setTerm(String term) {
+    this.term = term;
+  }
+
+  public String getDefinition() {
+    return definition;
+  }
+
+  public void setDefinition(String definition) {
+    this.definition = definition;
+  }
+
+  public Long getId() {
+    return id;
+  }
+}
---
+++ src/main/java/com/teamtreehouse/flashy/repositories/FlashCardRepository.java
@@ -0,0 +1,13 @@
+package com.teamtreehouse.flashy.repositories;
+
+import com.teamtreehouse.flashy.domain.FlashCard;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface FlashCardRepository extends CrudRepository<FlashCard, Long> {
+  List<FlashCard> findByIdNotIn(Collection<Long> ids);
+  List<FlashCard> findAll();
+}
