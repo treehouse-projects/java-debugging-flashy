99f939a6fda0e3b052dd3b34218e3b5c893e3cbb
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-08-17 23:30:59 +0300

    Refactor code to be more clear to read

--- src/main/java/com/teamtreehouse/flashy/services/FlashCardServiceImpl.java
+++ src/main/java/com/teamtreehouse/flashy/services/FlashCardServiceImpl.java
@@ -2,15 +2,15 @@ package com.teamtreehouse.flashy.services;
 
 import com.teamtreehouse.flashy.domain.FlashCard;
 import com.teamtreehouse.flashy.repositories.FlashCardRepository;
-
+import org.hibernate.mapping.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
+import java.util.Set;
 
 import static java.util.stream.Collectors.toList;
 
@@ -52,21 +52,20 @@ public class FlashCardServiceImpl implements FlashCardService {
   @Override
   public FlashCard getNextFlashCardBasedOnViews(Map<Long, Long> idToViewCounts) {
     FlashCard card = getNextUnseenFlashCard(idToViewCounts.keySet());
-    if (card != null) {
-      return card;
+    if (card == null) {
+      card = getLeastViewedFlashCard(idToViewCounts);
     }
+    return card;
+  }
+
+  public FlashCard getLeastViewedFlashCard(Map<Long, Long> idToViewCounts) {
     Long leastViewedId = null;
-    for (Map.Entry<Long, Long> entry : idToViewCounts.entrySet()) {
-      if (leastViewedId == null) {
-        leastViewedId = entry.getKey();
-        continue;
-      }
-      Long lowestScore = idToViewCounts.get(leastViewedId);
-      if (entry.getValue() < lowestScore) {
-        leastViewedId = entry.getKey();
-      }
-    }
-    return flashCardRepository.findOne(leastViewedId);
+    List<Map.Entry<Long, Long>> entries = new ArrayList<>(idToViewCounts.entrySet());
+    Collections.shuffle(entries);
+    return entries.stream()
+        .min(Comparator.comparing(Map.Entry::getValue))
+        .map(entry -> flashCardRepository.findOne(entry.getKey()))
+        .orElseThrow(IllegalArgumentException::new);
   }
 
   @Override
