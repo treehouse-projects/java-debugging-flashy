e4b6e955fb1cf8ee1687d830e1d372797c637f40
Anuraag Mishra <mishra.anuraag@yahoo.com>
2017-03-23 23:27:39 +0530

    Doing commit again to test push effect from intellij to github

--- src/main/java/com/teamtreehouse/flashy/controllers/IndexController.java
+++ src/main/java/com/teamtreehouse/flashy/controllers/IndexController.java
@@ -32,7 +32,7 @@ public class IndexController {
       }
     }
     Long totalCount = flashCardService.getCurrentCount();
-    if (totalCount > AMOUNT_TO_SHOW){
+    if (totalCount > AMOUNT_TO_SHOW ){
       ctaBuilder.append(" and ");
       ctaBuilder.append(totalCount - AMOUNT_TO_SHOW);
       ctaBuilder.append(" more");
