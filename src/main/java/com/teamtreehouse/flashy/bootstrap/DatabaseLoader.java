7641032086be9162032add832c130f7b5236f551
Boban Talevski <talevski.boban@gmail.com>
2017-12-08 15:00:39 +0100

    Removes the need for DatabaseLoader by implementing startup sql commands in import.sql and setting up DataConfig

--- src/main/java/com/teamtreehouse/flashy/bootstrap/DatabaseLoader.java
+++ src/main/java/com/teamtreehouse/flashy/bootstrap/DatabaseLoader.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+//@Component
 public class DatabaseLoader implements ApplicationRunner {
   @Autowired
   private FlashCardRepository flashCardRepository;
