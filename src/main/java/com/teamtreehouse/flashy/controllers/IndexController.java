52fb5d47880ec561b41a810ab7fa307052a095f9
Shoko Ishigaki <shoko.ishigaki@gmail.com>
2018-11-04 15:06:35 +0900

    Addresses issues #6 by changing the number of examples to 3

--- src/main/java/com/teamtreehouse/flashy/controllers/IndexController.java
+++ src/main/java/com/teamtreehouse/flashy/controllers/IndexController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @Controller
 public class IndexController {
+  public static final int AMOUNT_TO_SHOW = 3;
   private FlashCardService flashCardService;
 
   @Autowired
@@ -22,7 +23,7 @@ public class IndexController {
   @RequestMapping("/")
   public String index(Model model) {
     StringBuilder ctaBuilder = new StringBuilder();
-    List<FlashCard> cards = flashCardService.getRandomFlashCards(5);
+    List<FlashCard> cards = flashCardService.getRandomFlashCards(AMOUNT_TO_SHOW);
     ctaBuilder.append("Refresh your memory about ");
     for (FlashCard card : cards) {
       ctaBuilder.append(card.getTerm());
